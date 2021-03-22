3a05446a1b7b3243a12932c31f5435bbe95466c2
codeNameM1 <74464940+codeNameM1@users.noreply.github.com>
2021-03-22 15:23:04 -0400

    Add files via upload
    This is the layout I made, but I haven't learned how to add the additional info for the pages.

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p>
+    I created all the Projects on this page. While trying to learn how to
+    code from scratch.
+    </p>
+    <p>&copy; Created for <a href="#">CodeNameM1</a></p>
+</footer>
